8489816bcb77b67118b22a7d53fafc5ac82d9594
TobiasClausen <tobias.clausen06@gmail.com>
2024-02-14 14:02:21 +0100

    CountTheDevisibleNumbers

---
+++ src/main/java/CountTheDevisibleNumbers.java
@@ -0,0 +1,16 @@
+public class CountTheDevisibleNumbers {
+    public static long divisibleCount(long x, long y, long k) {
+
+        long countY = y / k;
+
+        long countX;
+        if (x>0){
+            countX= (x-1) / k;
+        }else{
+            countX=-1;
+        }
+
+
+        return countY - countX;
+    }
+}
