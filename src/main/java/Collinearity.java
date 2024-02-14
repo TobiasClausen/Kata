4a1274a533e150534e5e0948b0e4a31fd6b9f675
TobiasClausen <tobias.clausen06@gmail.com>
2024-02-14 13:47:20 +0100

    Collinaerity

--- src/main/java/Collinearity.java
+++ src/main/java/Collinearity.java
@@ -1,2 +1,13 @@
+
+
+
 public class Collinearity {
+    public static boolean collinearity(int x1, int y1, int x2, int y2) {
+        int result = x1 * y2 - x2 * y1;
+
+        return result == 0;
+    }
 }
+
+
+
