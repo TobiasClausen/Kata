85b712d65ed63dc28d4783270d458e7bc2f5069b
TobiasClausen <tobias.clausen06@gmail.com>
2024-02-15 08:15:39 +0100

    ConstructAGraph verbessert

--- src/main/java/ConstructAGraph.java
+++ src/main/java/ConstructAGraph.java
@@ -1,50 +1,61 @@
 import java.awt.Point;
 
 public class ConstructAGraph {
+
+    public static final char CHARZERO = '+';
+    public static final char CHARYAXIS = '|';
+    public static final char CHARXAXIS = '-';
+    public static final char CHARPOINT = '*';
+    public static final char CHARNOTHING = ' ';
+
     public static char[][] build(Point[] points) {
-        int minX = 0, maxX = 0, minY = 0, maxY = 0;
-        char charzero = '+';
-        char charxaxis = '-';
-        char charyaxis = '|';
-        char charpoint = '*';
-        char charnothing = ' ';
+        Graphdimensions graphdimensions = new Graphdimensions(points);
 
-        for (Point point : points) {
-            minX = Math.min(minX, point.x);
-            maxX = Math.max(maxX, point.x);
-            minY = Math.min(minY, point.y);
-            maxY = Math.max(maxY, point.y);
-        }
 
-        int width = maxX - minX + 1;
-        int height = maxY - minY + 1;
+        int width = graphdimensions.getMaxX() - graphdimensions.getMinX() + 1;
+        int height = graphdimensions.getMaxY() - graphdimensions.getMinY() + 1;
 
         char[][] graph = new char[height][width];
 
-        for (int y = 0; y<height; y++) {
-            for (int x = 0; x<width; x++){
-                graph[y][x] = charnothing;
-            }
-        }
+        fillUpWholearray(width, height, graph);
 
-        for (int x = minX; x <= maxX; x++) {
-            if (minY <= 0 && 0 <= maxY) {
-                graph[maxY][x - minX] = (x == 0) ? charzero : charxaxis;
-            }
-        }
-
-        for (int y = minY; y <= maxY; y++) {
-            if (minX <= 0 && 0 <= maxX) {
-                graph[maxY - y][0 - minX] = (y == 0) ? charzero : charyaxis;
-            }
-        }
-
-        for (Point point : points) {
-            int x = point.x - minX;
-            int y = maxY - point.y;
-            graph[y][x] = charpoint;
-        }
+        drawXAxis(graphdimensions, graph);
+        drawYAxis(graphdimensions, graph);
+        fillGraphWithPoints(points, graphdimensions, graph);
 
         return graph;
+    }
+
+    private static void fillUpWholearray(int width, int height, char[][] graph) {
+        for (int y = 0; y< height; y++) {
+            for (int x = 0; x< width; x++){
+                graph[y][x] = CHARNOTHING;
+            }
+        }
+    }
+
+    private static void drawXAxis(Graphdimensions graphdimensions, char[][] graph) {
+        for (int x = graphdimensions.getMinX(); x <= graphdimensions.getMaxX(); x++) {
+            if (graphdimensions.getMinY() <= 0 && 0 <= graphdimensions.getMaxY()) {
+                graph[graphdimensions.getMaxY()][x - graphdimensions.getMinX()] = (x == 0) ? CHARZERO : CHARXAXIS;
+            }
+        }
+    }
+
+    private static void drawYAxis(Graphdimensions graphdimensions, char[][] graph) {
+        for (int y = graphdimensions.getMinY(); y <= graphdimensions.getMaxY(); y++) {
+            if (graphdimensions.getMinX() <= 0 && 0 <= graphdimensions.getMaxX()) {
+                graph[graphdimensions.getMaxY() - y][0 - graphdimensions.getMinX()] = (y == 0) ? CHARZERO : CHARYAXIS;
+            }
+        }
+    }
+
+
+    private static void fillGraphWithPoints(Point[] points, Graphdimensions graphdimensions, char[][] graph) {
+        for (Point point : points) {
+            int x = point.x - graphdimensions.getMinX();
+            int y = graphdimensions.getMaxY() - point.y;
+            graph[y][x] = CHARPOINT;
+        }
     }
 }
---
+++ src/main/java/Graphdimensions.java
@@ -0,0 +1,41 @@
+import java.awt.*;
+
+public class Graphdimensions {
+    private Point[] points;
+    private int minX= 0;
+    private int maxX = 0;
+    private int minY = 0;
+    private int maxY = 0;
+
+
+    public Graphdimensions(Point[] points) {
+
+        this.points = points;
+
+        for (Point point : points) {
+            minX = Math.min(minX, point.x);
+            maxX = Math.max(maxX, point.x);
+            minY = Math.min(minY, point.y);
+            maxY = Math.max(maxY, point.y);
+        }
+    }
+
+    public int getMinX() {
+        return minX;
+    }
+
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public int getMinY() {
+        return minY;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+}
+
+
+
